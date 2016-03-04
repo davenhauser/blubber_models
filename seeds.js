722794260011212ba818d9aa3888f669c99a2e0c
david niederhauser <niederhauser.dave@gmail.com>
2016-03-03 16:55:09 -0800

    add spikefile to search threads

---
+++ seeds.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/blubber_app");
+
+var User    = require("./models/User"),
+    Thread = require("./models/Thread");
+
+Thread.remove({}, function(err,results){
+  User.remove({}, function(err, results) {
+    User.create([
+        {name: "John Marshall",             email: "jm@us.courts.gov",   moderator: true},
+        {name: "Oliver Wendell Holmes Jr.", email: "owh2@us.courts.gov"},
+        {name: "Thurgood Marshall",         email: "tm@us.courts.gov"},
+        {name: "Sandra Day O'Connor",       email: "sdo@us.courts.gov"}
+      ], function(err, users) {
+        if (err) console.log(err);
+
+        // console.log(users);
+
+        var john = users[0];
+        //create threads
+        Thread.create(
+          {name: "YOLO", creator: john },
+          function(err, results){
+            if (err) console.log(err);
+            console.log(results);
+
+            mongoose.connection.close();
+        });
+    });
+  });
+
+});
+
+
+
+
---
+++ spike.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/blubber_app");
+
+var User    = require("./models/User"),
+    Thread = require("./models/Thread");
+
+
+var threads = Thread.find({}, function(err, threads){
+  console.log(threads);
+
+  mongoose.connection.close();
+});
+
+
+
+
